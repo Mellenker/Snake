f85c063c09adb68c4ba65b08a4225957ba5cb9c2
a22melst <a22melst@student.his.se>
2025-01-08 12:18:59 +0100

    Added keyboard input

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,22 +1,48 @@
 #include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+using namespace std;
 
-    while (window.isOpen())
-    {
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Snake");
+    sf::CircleShape player(100.f);
+    player.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Handle one-time actions (Pause maybe?)
+            if (event.type == sf::Event::KeyPressed) {
+                switch (event.key.code) {
+                    case sf::Keyboard::Key::Escape:
+                        cout << "PAUSE" << endl;
+                        break;
+                }
+            }
         }
 
+        // Player movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			player.move(0, -0.1f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            player.move(-0.1f, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            player.move(0, 0.1f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            player.move(0.1f, 0);
+        }
+
+
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
 
